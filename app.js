3def82c7b4020f5892db1d6cda2434249bfdaec0
MitsonMars <immits2x2@gmail.com>
2025-09-28 23:12:50 -0400

    Update app.js
    11.45 minute mark of tutorial

--- app.js
+++ app.js
@@ -1,1 +1,18 @@
+const taskForm = document.getElementById("task-form");
+const taskList = document.getElementById("task-list");
 
+// console.log(taskForm, taskList);
+taskForm.addEventListener("submit", function (event){ 
+  event.preventDefault();
+  const taskInput = document.getElementById("task-input"); 
+  const taskText = taskInput.value.trim(); 
+
+  if(taskText !== ""){
+    //Create a new task item 
+    const taskItem = document.createElement("li"); 
+    taskItem.classlist.add("task-item"); 
+    taskItem.textContent = taskText;
+    taskList.appendChild(taskItem);
+  }
+//console.log(taskText); 
+}); 
